f0a53003335680ab4e201a6e7a37185bedad0bd1
Divya <drmohan@andrew.cmu.edu>
2016-04-17 18:10:50 -0400

    has_secure_password:

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   should belong_to(:employee)
+  should have_secure_password
 
   # Validating email...
   should allow_value("fred@fred.com").for(:email)
