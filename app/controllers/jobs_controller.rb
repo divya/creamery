6d1d5abf583a7c404300ec65c42e3e7213090193
Divya <drmohan@andrew.cmu.edu>
2016-04-18 21:57:16 -0400

    job controller

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,52 @@
+class JobsController < ApplicationController
+  before_action :set_job, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @active_jobs = Job.active.alphabetical.paginate(page: params[:page]).per_page(10)
+    @inactive_jobs = Job.inactive.alphabetical.paginate(page: params[:page]).per_page(10)  
+  end
+
+  def show
+    #@current_assignments = @store.assignments.current.by_employee.paginate(page: params[:page]).per_page(8)
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def edit
+  end
+
+  def create
+    @job = Job.new(job_params)
+    
+    if @job.save
+      redirect_to job_path(@job), notice: "Successfully created #{@job.name}."
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @job.update(job_params)
+      redirect_to job_path(@job), notice: "Successfully updated #{@job.name}."
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to jobs_path, notice: "Successfully removed #{@job.name} from the AMC system."
+  end
+
+  private
+  def set_job
+    @job = Job.find(params[:id])
+  end
+
+  def job_params
+    params.require(:job).permit(:name, :description, :active)
+  end
+
+end
